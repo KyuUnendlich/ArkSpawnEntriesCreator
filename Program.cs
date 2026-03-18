e5b8b07f70ec11fae0adcd080176ec9ac519e90e
Unendlich <34223835+KyuUnendlich@users.noreply.github.com>
2026-03-18 22:18:03 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -807,12 +807,12 @@ namespace ArkSpawnEntriesCreator
             NeoEuropa, //Europa
             FerasDinocroc,
             FerasSmilo, //GreaterSaber
-            FerasFoxes,
+            FerasFoxes, //Arcticfox
             CuriousCryptids, //TutorialDino
-            AstraeosMythCreatures,
-            MyrmDracoteuthis,
+            AstraeosCreatures,
+            MyrmDracoteuthis, //Dracoteuthis
             MyrmDraconisGlaucus, //Glaucus
-            IsleSkyshroud,
+            IsleSkyshroud, //skyshroud
             IsleOxalaia,
 
 
@@ -826,7 +826,7 @@ namespace ArkSpawnEntriesCreator
         //const string Path = "C:/Users/matth/Desktop/Ascended/AtlasFish.txt";
         const bool replaceFile = true;
         const bool secondString2 = false;
-        const string Path = "E:/ARK Saves/ArkSpawnEntriesCreator/AscendedModsAdditions/NeoEuropa.txt";
+        const string Path = "E:/ARK Saves/ArkSpawnEntriesCreator/AscendedModsAdditions/FerasFoxes.txt";
     }
 
 
